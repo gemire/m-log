336088b51cb88cdb70bf1997d70f63e96284ce14
gaoyoubo@gmail.com <gaoyoubo@gmail.com@fb20f3ff-b375-a5d1-e911-89717aad2d21>
2012-12-31 09:18:48 +0000

    去掉cookie的base64加密

--- mspring-platform/src/main/java/org/mspring/platform/utils/CookieUtils.java
+++ mspring-platform/src/main/java/org/mspring/platform/utils/CookieUtils.java
@@ -35,7 +35,8 @@ public class CookieUtils {
 
     public static void setCookie(HttpServletResponse response, String name, String value, int iExpireDays) {
         try {
-            Cookie cookie = new Cookie(name, StringUtils.encodeBASE64(value));
+            //Cookie cookie = new Cookie(name, StringUtils.encodeBASE64(value));
+            Cookie cookie = new Cookie(name, value);
             if (iExpireDays != 0) {
                 cookie.setMaxAge(iExpireDays * 24 * 60 * 60 * 1000);
             }
@@ -57,7 +58,8 @@ public class CookieUtils {
         for (int i = 0; i < cookies.length; i++) {
             Cookie cookie = cookies[i];
             if (name.equals(cookie.getName())) {
-                return new StringUtils().decodeBASE64(cookie.getValue());
+                //return new StringUtils().decodeBASE64(cookie.getValue());
+                return cookie.getValue();
             }
         }
         return "";
