2c4cb52feceaedaa046944f93e17120802646e6e
gaoyoubo@gmail.com <gaoyoubo@gmail.com@fb20f3ff-b375-a5d1-e911-89717aad2d21>
2013-01-28 06:59:44 +0000

    解决固定连接中月份少一个月的问题。

--- M-LOG-standard-1.3/src/org/mspring/mlog/utils/PermaLinkUtils.java
+++ M-LOG-standard-1.3/src/org/mspring/mlog/utils/PermaLinkUtils.java
@@ -141,7 +141,7 @@ public class PermaLinkUtils {
 
     public final static String getDefaultPostURL() {
         Calendar cal = Calendar.getInstance();
-        String url = String.format("/post/%s/%s/%s/%s.html", new Object[] { cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), cal.getTimeInMillis() });
+        String url = String.format("/post/%s/%s/%s/%s.html", new Object[] { cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1, cal.get(Calendar.DAY_OF_MONTH), cal.getTimeInMillis() });
         log.debug("Get permalinks [" + url + "]");
         return url;
     }
