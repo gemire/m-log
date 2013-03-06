978819e3054edcb205763b41d4119d11f982cdcc
gaoyoubo@gmail.com <gaoyoubo@gmail.com@fb20f3ff-b375-a5d1-e911-89717aad2d21>
2013-03-06 07:55:42 +0000

    优化邮件发送方式，异步发送邮件

--- trunk/M-LOG-standard-1.3/src/org/mspring/platform/task/AbstractTask.java
+++ trunk/M-LOG-standard-1.3/src/org/mspring/platform/task/AbstractTask.java
@@ -5,37 +5,25 @@ package org.mspring.platform.task;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallbackWithoutResult;
-import org.springframework.transaction.support.TransactionTemplate;
-
 /**
  * @author Gao Youbo
  * @since Feb 20, 2012
  */
 public abstract class AbstractTask implements Task {
-    protected PlatformTransactionManager transactionManager;
 
-    @Autowired
-    public void setTransactionManager(PlatformTransactionManager transactionManager) {
-        this.transactionManager = transactionManager;
-    }
-
-    @Async
+    // @Async
     public void doAsyncTask(final Map<Object, Object> context) {
-        new TransactionTemplate(this.transactionManager).execute(new TransactionCallbackWithoutResult() {
-            protected void doInTransactionWithoutResult(TransactionStatus status) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
                 try {
                     AbstractTask.this.doTask(context);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
-        });
+        }).start();
     }
 
     public void doSyncTask(Map<Object, Object> context) {
