46ef4bbbbb68e5ae782d4f629ada26702eb78382
gaoyoubo@gmail.com <gaoyoubo@gmail.com@fb20f3ff-b375-a5d1-e911-89717aad2d21>
2013-03-11 02:58:23 +0000

    添加采集工具API

---
+++ trunk/M-LOG-standard-1.3/src/org/mspring/mlog/api/spider/widget/SpiderRuleWidget.java
@@ -0,0 +1,26 @@
+///**
+// * 
+// */
+//package org.mspring.mlog.api.spider.widget;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import org.mspring.mlog.web.freemarker.widget.stereotype.Widget;
+//import org.mspring.mlog.web.module.admin.AbstractAdminWidget;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+///**
+// * @author Gao Youbo
+// * @since 2013-3-11
+// * @description
+// * @TODO
+// */
+//@Widget
+//@RequestMapping("/admin/spider/rule")
+//public class SpiderRuleWidget extends AbstractAdminWidget {
+//    public String list(HttpServletRequest request, HttpServletResponse response, Model model) {
+//
+//    }
+//}
