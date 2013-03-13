3938a1be71e56872bd555ae50c601df53644380b
huhongyu416@gmail.com <huhongyu416@gmail.com@fb20f3ff-b375-a5d1-e911-89717aad2d21>
2013-03-13 02:01:32 +0000

    tag自动补全功能-多个标签提示补全

--- M-LOG-standard-1.3/src/org/mspring/mlog/web/module/admin/PostWidget.java
+++ M-LOG-standard-1.3/src/org/mspring/mlog/web/module/admin/PostWidget.java
@@ -4,6 +4,7 @@
 package org.mspring.mlog.web.module.admin;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -48,194 +49,251 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Widget
 @RequestMapping("/admin/post")
 public class PostWidget extends AbstractAdminWidget {
-    private static final Logger log = Logger.getLogger(PostWidget.class);
+	private static final Logger log = Logger.getLogger(PostWidget.class);
 
-    @Autowired
-    private PostService postService;
-    @Autowired
-    private CatalogService catalogService;
-    @Autowired
-    private TagService tagService;
+	@Autowired
+	private PostService postService;
+	@Autowired
+	private CatalogService catalogService;
+	@Autowired
+	private TagService tagService;
 
-    @SuppressWarnings("rawtypes")
-    @RequestMapping("/list")
-    @Log
-    public String listPost(@ModelAttribute Page<Post> postPage, @ModelAttribute Post post, @QueryParam Map queryParams, HttpServletRequest request, HttpServletResponse response, Model model) {
-        if (postPage == null) {
-            postPage = new Page<Post>();
-        }
-        postPage.setSort(new Sort("isTop desc, id desc", ""));
+	@SuppressWarnings("rawtypes")
+	@RequestMapping("/list")
+	@Log
+	public String listPost(@ModelAttribute Page<Post> postPage,
+			@ModelAttribute Post post, @QueryParam Map queryParams,
+			HttpServletRequest request, HttpServletResponse response,
+			Model model) {
+		if (postPage == null) {
+			postPage = new Page<Post>();
+		}
+		postPage.setSort(new Sort("isTop desc, id desc", ""));
 
-        // 默认查看已发布的文章
-        if (post == null) {
-            post = new Post();
-        }
-        if (queryParams.get("status") == null || StringUtils.isBlank(queryParams.get("status").toString())) {
-            post.setStatus(Post.Status.PUBLISH);
-            queryParams.put("status", Post.Status.PUBLISH);
-        }
+		// 默认查看已发布的文章
+		if (post == null) {
+			post = new Post();
+		}
+		if (queryParams.get("status") == null
+				|| StringUtils.isBlank(queryParams.get("status").toString())) {
+			post.setStatus(Post.Status.PUBLISH);
+			queryParams.put("status", Post.Status.PUBLISH);
+		}
 
-        postPage = postService.findPost(postPage, new PostQueryCriterion(queryParams));
-        model.addAttribute("postPage", postPage);
-        model.addAttribute("status", Post.Status.getStatusMap());
-        return "/admin/post/listPost";
-    }
+		postPage = postService.findPost(postPage, new PostQueryCriterion(
+				queryParams));
+		model.addAttribute("postPage", postPage);
+		model.addAttribute("status", Post.Status.getStatusMap());
+		return "/admin/post/listPost";
+	}
 
-    @RequestMapping("/create")
-    public String createPostView(@ModelAttribute Post post, HttpServletRequest request, HttpServletResponse response, Model model) {
-        // 文章分类
-        List<Catalog> catalogs = catalogService.findAllCatalog();
-        model.addAttribute("catalogs", catalogs);
+	@RequestMapping("/create")
+	public String createPostView(@ModelAttribute Post post,
+			HttpServletRequest request, HttpServletResponse response,
+			Model model) {
+		// 文章分类
+		List<Catalog> catalogs = catalogService.findAllCatalog();
+		model.addAttribute("catalogs", catalogs);
 
-        // 是否开启评论
-        Map<String, String> commentStatus = new HashMap<String, String>();
-        commentStatus.put(Post.CommentStatus.OPEN, "开启");
-        commentStatus.put(Post.CommentStatus.CLOSE, "关闭");
-        model.addAttribute("commentStatus", commentStatus);
+		// 是否开启评论
+		Map<String, String> commentStatus = new HashMap<String, String>();
+		commentStatus.put(Post.CommentStatus.OPEN, "开启");
+		commentStatus.put(Post.CommentStatus.CLOSE, "关闭");
+		model.addAttribute("commentStatus", commentStatus);
 
-        Map<String, String> isTop = new HashMap<String, String>();
-        isTop.put("true", "是");
-        isTop.put("false", "否");
-        model.addAttribute("isTop", isTop);
-        return "/admin/post/createPost";
-    }
+		Map<String, String> isTop = new HashMap<String, String>();
+		isTop.put("true", "是");
+		isTop.put("false", "否");
+		model.addAttribute("isTop", isTop);
+		return "/admin/post/createPost";
+	}
 
-    @RequestMapping("/create/save")
-    @Log
-    public String create_save(@ModelAttribute Post post, HttpServletRequest request, HttpServletResponse response, Model model) {
-        User user = SecurityUtils.getCurrentUser(request);
-        if (post.getAuthor() == null) {
-            post.setAuthor(user);
-        }
-        post.setPostIp(request.getRemoteAddr());
-        postService.createPost(post);
-        return "redirect:/admin/post/list";
-    }
+	@RequestMapping("/create/save")
+	@Log
+	public String create_save(@ModelAttribute Post post,
+			HttpServletRequest request, HttpServletResponse response,
+			Model model) {
+		User user = SecurityUtils.getCurrentUser(request);
+		if (post.getAuthor() == null) {
+			post.setAuthor(user);
+		}
+		post.setPostIp(request.getRemoteAddr());
+		postService.createPost(post);
+		return "redirect:/admin/post/list";
+	}
 
-    /**
-     * 将文章移入回收站
-     * 
-     * @param id
-     * @param request
-     * @param response
-     * @param model
-     * @return
-     */
-    @RequestMapping("/trash")
-    @Log
-    public String trashPost(@RequestParam(required = false) Long[] id, @ModelAttribute Page<Post> postPage, @ModelAttribute Post post, @QueryParam Map queryParams, HttpServletRequest request, HttpServletResponse response, Model model) {
-        if (id != null && id.length > 0) {
-            postService.discardPost(id);
-        }
-        return listPost(postPage, post, queryParams, request, response, model);
-    }
+	/**
+	 * 将文章移入回收站
+	 * 
+	 * @param id
+	 * @param request
+	 * @param response
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/trash")
+	@Log
+	public String trashPost(@RequestParam(required = false) Long[] id,
+			@ModelAttribute Page<Post> postPage, @ModelAttribute Post post,
+			@QueryParam Map queryParams, HttpServletRequest request,
+			HttpServletResponse response, Model model) {
+		if (id != null && id.length > 0) {
+			postService.discardPost(id);
+		}
+		return listPost(postPage, post, queryParams, request, response, model);
+	}
 
-    /**
-     * 从回收站还原文章
-     * 
-     * @param id
-     * @param request
-     * @param response
-     * @param model
-     * @return
-     */
-    @RequestMapping("/trash2Publish")
-    @Log
-    public String trash2Publish(@RequestParam(required = false) Long[] id, @ModelAttribute Page<Post> postPage, @ModelAttribute Post post, @QueryParam Map queryParams, HttpServletRequest request, HttpServletResponse response, Model model) {
-        if (id != null && id.length > 0) {
-            postService.trash2Publish(id);
-        }
-        // return "redirect:/admin/post/list";
-        return listPost(postPage, post, queryParams, request, response, model);
-    }
+	/**
+	 * 从回收站还原文章
+	 * 
+	 * @param id
+	 * @param request
+	 * @param response
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/trash2Publish")
+	@Log
+	public String trash2Publish(@RequestParam(required = false) Long[] id,
+			@ModelAttribute Page<Post> postPage, @ModelAttribute Post post,
+			@QueryParam Map queryParams, HttpServletRequest request,
+			HttpServletResponse response, Model model) {
+		if (id != null && id.length > 0) {
+			postService.trash2Publish(id);
+		}
+		// return "redirect:/admin/post/list";
+		return listPost(postPage, post, queryParams, request, response, model);
+	}
 
-    /**
-     * 彻底删除文章
-     * 
-     * @param id
-     * @param request
-     * @param response
-     * @param model
-     * @return
-     */
-    @RequestMapping("/delete")
-    @Log
-    public String deletePost(@RequestParam(required = false) Long[] id, @ModelAttribute Page<Post> postPage, @ModelAttribute Post post, @QueryParam Map queryParams, HttpServletRequest request, HttpServletResponse response, Model model) {
-        if (id != null && id.length > 0) {
-            postService.deletePost(id);
-        }
-        return listPost(postPage, post, queryParams, request, response, model);
-    }
+	/**
+	 * 彻底删除文章
+	 * 
+	 * @param id
+	 * @param request
+	 * @param response
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/delete")
+	@Log
+	public String deletePost(@RequestParam(required = false) Long[] id,
+			@ModelAttribute Page<Post> postPage, @ModelAttribute Post post,
+			@QueryParam Map queryParams, HttpServletRequest request,
+			HttpServletResponse response, Model model) {
+		if (id != null && id.length > 0) {
+			postService.deletePost(id);
+		}
+		return listPost(postPage, post, queryParams, request, response, model);
+	}
 
-    /**
-     * 清空回收站
-     * 
-     * @return
-     */
-    @RequestMapping("clearTrash")
-    @Log
-    public String clearTrash(@ModelAttribute Page<Post> postPage, @ModelAttribute Post post, @QueryParam Map queryParams, HttpServletRequest request, HttpServletResponse response, Model model) {
-        postService.clearTrash();
-        return listPost(postPage, post, queryParams, request, response, model);
-    }
+	/**
+	 * 清空回收站
+	 * 
+	 * @return
+	 */
+	@RequestMapping("clearTrash")
+	@Log
+	public String clearTrash(@ModelAttribute Page<Post> postPage,
+			@ModelAttribute Post post, @QueryParam Map queryParams,
+			HttpServletRequest request, HttpServletResponse response,
+			Model model) {
+		postService.clearTrash();
+		return listPost(postPage, post, queryParams, request, response, model);
+	}
 
-    @RequestMapping("/edit")
-    public String editPostView(@ModelAttribute Post post, HttpServletRequest request, HttpServletResponse response, Model model) {
-        if (post == null || post.getId() == null) {
-            Object obj = getSessionAttribute(request, "PostWidget_edit_id");
-            if (obj != null) {
-                Long id = (Long) obj;
-                post = new Post(id);
-            }
-        }
-        setSessionAttribute(request, "PostWidget_edit_id", post.getId());
+	@RequestMapping("/edit")
+	public String editPostView(@ModelAttribute Post post,
+			HttpServletRequest request, HttpServletResponse response,
+			Model model) {
+		if (post == null || post.getId() == null) {
+			Object obj = getSessionAttribute(request, "PostWidget_edit_id");
+			if (obj != null) {
+				Long id = (Long) obj;
+				post = new Post(id);
+			}
+		}
+		setSessionAttribute(request, "PostWidget_edit_id", post.getId());
 
-        if (post == null || post.getId() == null) {
-            return prompt(model, "请先选择要修改的文章");
-        }
-        post = postService.getPostById(post.getId());
+		if (post == null || post.getId() == null) {
+			return prompt(model, "请先选择要修改的文章");
+		}
+		post = postService.getPostById(post.getId());
 
-        Map<String, String> isTop = new HashMap<String, String>();
-        isTop.put("true", "是");
-        isTop.put("false", "否");
-        model.addAttribute("isTop", isTop);
-        model.addAttribute("post", post); // 文章
-        model.addAttribute("catalogs", catalogService.findAllCatalog()); // 文章分类
-        model.addAttribute("commentStatus", Post.CommentStatus.getCommentStatusMap()); // 是否开启评论
-        return "/admin/post/editPost";
-    }
+		Map<String, String> isTop = new HashMap<String, String>();
+		isTop.put("true", "是");
+		isTop.put("false", "否");
+		model.addAttribute("isTop", isTop);
+		model.addAttribute("post", post); // 文章
+		model.addAttribute("catalogs", catalogService.findAllCatalog()); // 文章分类
+		model.addAttribute("commentStatus",
+				Post.CommentStatus.getCommentStatusMap()); // 是否开启评论
+		return "/admin/post/editPost";
+	}
 
-    @RequestMapping("/edit/save")
-    @Log
-    public String edit_save(@ModelAttribute Post post, HttpServletRequest request, HttpServletResponse response, Model model) {
-        postService.updatePost(post);
-        return "redirect:/admin/post/list";
-    }
+	@RequestMapping("/edit/save")
+	@Log
+	public String edit_save(@ModelAttribute Post post,
+			HttpServletRequest request, HttpServletResponse response,
+			Model model) {
+		postService.updatePost(post);
+		return "redirect:/admin/post/list";
+	}
 
-    /**
-     * 更新文章索引
-     * 
-     * @return
-     */
-    @RequestMapping("/updateIndex")
-    @ResponseBody
-    @Log
-    public String updateLuceneIndex() {
-        try {
-            HibernateSearchService hibernateSearchService = ServiceFactory.getHibernateSearchService();
-            hibernateSearchService.updateAllIndex(Post.class);
-        } catch (Exception e) {
-            // TODO: handle exception
-            log.debug("update post index failure!", e);
-            return "false";
-        }
-        return "true";
-    }
+	/**
+	 * 更新文章索引
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/updateIndex")
+	@ResponseBody
+	@Log
+	public String updateLuceneIndex() {
+		try {
+			HibernateSearchService hibernateSearchService = ServiceFactory
+					.getHibernateSearchService();
+			hibernateSearchService.updateAllIndex(Post.class);
+		} catch (Exception e) {
+			// TODO: handle exception
+			log.debug("update post index failure!", e);
+			return "false";
+		}
+		return "true";
+	}
 
-    @RequestMapping(value = "/autocomplete")
-    public void autocomplete(@PathParam(required = false) String keyword, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        List<Tag> tags = tagService.findLikeByName(keyword);
-        JSONRender render = new JSONRender(tags, false);
-        render.render(response);
-    }
+	/**
+	 * 标签自动补全
+	 * 
+	 * @param keyword
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	@RequestMapping(value = "/autocomplete")
+	public void autocomplete(@PathParam(required = false) String keyword,
+			HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String key ="";
+		String prefixKey="";
+		prefixKey = keyword.substring(0, keyword.lastIndexOf(",")+1);
+		
+		key = keyword.substring(keyword.lastIndexOf(",")+1, keyword.length());
+		List<Tag> tags = new ArrayList<Tag>();
+		if(key!=null&&!key.equals("")){
+			tags = tagService.findLikeByName(key);
+			for (Tag tag : tags) {
+				tag.setName(prefixKey+tag.getName());
+			}
+		}
+		
+		JSONRender render = new JSONRender(tags, false);
+		render.render(response);
+	}
+	
+	public static void main(String[] args) {
+		String key="你好";
+		key = key.substring(key.lastIndexOf(",")+1, key.length());
+		//key = key.substring(0, key.lastIndexOf(",")+1);
+		System.out.println(key);
+	}
 }
