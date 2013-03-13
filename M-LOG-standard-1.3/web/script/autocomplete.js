21ba04bd2d246f8520fdf5abb292bb38b647f12c
huhongyu416@gmail.com <huhongyu416@gmail.com@fb20f3ff-b375-a5d1-e911-89717aad2d21>
2013-03-13 05:53:58 +0000

    tag自动补全功能-多个标签提示补全

--- M-LOG-standard-1.3/web/script/autocomplete.js
+++ M-LOG-standard-1.3/web/script/autocomplete.js
@@ -1,26 +1,49 @@
 $(function() {
-		$("#tags").autocomplete({ 
-				minLength: 1,
-		        source: function (request, response) {
-				            $.ajax({
-				                type: "POST",
-				                url: mlog.variable.base+"/admin/post/autocomplete?keyword=" + encodeURIComponent(request.term),
-				                contentType: "application/json; charset=utf-8",
-				                dataType: "json",
-				                success: function (data) {
-			            			response($.map(data, function (item) {
-				                        return { label: (item.name.toString()).substring((item.name.toString()).lastIndexOf(",")+1,(item.name.toString()).length), value: item.name, id: item.id };
-				                    }));
-				                },
-				                error: function () {
-				                    alert("补全失败");
-				                }
-				            });
-		        		},
-				select: function(e, ui) { 
-		        			
-						} 
-			});
+    function split( val ) {
+      return val.split( /,\s*/ );
+    }
+    function extractLast( term ) {
+      return split( term ).pop();
+    }
+ 
+    $( "#tags" )
+      // don't navigate away from the field on tab when selecting an item
+      .bind( "keydown", function( event ) {
+        if ( event.keyCode === $.ui.keyCode.TAB &&
+            $( this ).data( "ui-autocomplete" ).menu.active ) {
+          event.preventDefault();
+        }
+      })
+      .autocomplete({
+        minLength: 0,
+        source: function (request, response) {
+            $.ajax({
+                type: "POST",
+                url: mlog.variable.base+"/admin/post/autocomplete?keyword=" +  extractLast( request.term ) ,
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (data) {
+                	response( $.ui.autocomplete.filter(
+                			data, extractLast( request.term ) ) );
+                },
+                error: function () {
+                    alert("补全失败");
+                }
+            });
+		},
+        focus: function() {
+          // prevent value inserted on focus
+          return false;
+        },
+        select: function( event, ui ) {
+          var terms = split( this.value );
+          terms.pop();
+          terms.push( ui.item.value );
+          terms.push( "" );
+          this.value = terms.join( ", " );
+          return false;
+        }
+      });
+  });
 
-});
 
