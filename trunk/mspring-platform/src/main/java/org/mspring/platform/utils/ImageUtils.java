5963e511a9baebd7bac8e6d96092ca6d74fe211b
gaoyoubo@gmail.com <gaoyoubo@gmail.com@fb20f3ff-b375-a5d1-e911-89717aad2d21>
2013-06-03 09:13:53 +0000

    优化图片操作API，添加图片剪裁方法

--- trunk/mspring-platform/src/main/java/org/mspring/platform/utils/ImageUtils.java
+++ trunk/mspring-platform/src/main/java/org/mspring/platform/utils/ImageUtils.java
@@ -3,8 +3,6 @@
  */
 package org.mspring.platform.utils;
 
-import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.geom.AffineTransform;
@@ -18,11 +16,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.Random;
 
 import javax.imageio.ImageIO;
 
-import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.commons.imaging.ImageReadException;
+import org.apache.commons.imaging.Imaging;
+import org.apache.commons.imaging.common.IImageMetadata;
 
 import com.gif4j.GifDecoder;
 import com.gif4j.GifEncoder;
@@ -36,6 +35,17 @@ import com.mortennobel.imagescaling.ResampleOp;
  * @since Feb 20, 2012
  */
 public class ImageUtils {
+
+    public static final class ImageType {
+        public static final String PNG = "PNG";
+        public static final String JPG = "JPG";
+        public static final String JPEG = "JPEG";
+        public static final String BMP = "BMP";
+        public static final String GIF = "GIF";
+
+        public static final String[] types = { PNG, JPG, JPEG, BMP, GIF };
+    }
+
     /**
      * 判断是否为图片
      * 
@@ -43,46 +53,12 @@ public class ImageUtils {
      * @return
      */
     public static boolean isImage(String extendName) {
-        return "png".equalsIgnoreCase(extendName) || "jpg".equalsIgnoreCase(extendName) || "jpeg".equalsIgnoreCase(extendName) || "bmp".equalsIgnoreCase(extendName) || "gif".equalsIgnoreCase(extendName);
-    }
-
-    /**
-     * 判断是否为JPG图片
-     * 
-     * @param fn
-     * @return
-     */
-    public static boolean isJPG(String fn) {
-        if (fn == null)
-            return false;
-        String s_fn = fn.toLowerCase();
-        return s_fn.endsWith("jpg") || s_fn.endsWith("jpge");
-    }
-
-    /**
-     * 判断是否为JPG图片
-     * 
-     * @param fn
-     * @return
-     */
-    public static boolean isBMP(String fn) {
-        if (fn == null)
-            return false;
-        String s_fn = fn.toLowerCase();
-        return s_fn.endsWith("bmp");
-    }
-
-    /**
-     * 判断图片是否是gif
-     * 
-     * @param fn
-     * @return
-     */
-    public static boolean isGif(String fn) {
-        if (fn == null)
-            return false;
-        String s_fn = fn.toLowerCase();
-        return s_fn.endsWith("gif");
+        for (String type : ImageType.types) {
+            if (type.equalsIgnoreCase(extendName)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
@@ -130,16 +106,16 @@ public class ImageUtils {
      */
     public static void saveImage(File formImage, File saveImage, int maxWidth, int maxHeight) throws IOException {
         BufferedImage srcImage;
-        String imgType = "JPEG";
-        if (formImage.getName().toLowerCase().endsWith(".png")) {
-            imgType = "PNG";
+        String imgType = ImageType.JPG;
+        if (formImage.getName().toUpperCase().endsWith(ImageType.PNG)) {
+            imgType = ImageType.PNG;
         }
         // 如果保存路径不存在，那么创建
         if (!saveImage.getParentFile().exists()) {
             saveImage.getParentFile().mkdirs();
         }
-        // // 处理GIF动态图片
-        if (formImage.getName().toLowerCase().endsWith(".gif")) {
+        // 处理GIF动态图片
+        if (formImage.getName().toUpperCase().endsWith(ImageType.GIF)) {
             GifImage gifImage = GifDecoder.decode(formImage);
             GifImage newGif = GifTransformer.resize(gifImage, maxWidth, maxHeight, false);
             GifEncoder.encode(newGif, saveImage);
@@ -195,8 +171,9 @@ public class ImageUtils {
             WritableRaster raster = cm.createCompatibleWritableRaster(targetW, targetH);
             boolean alphaPremultiplied = cm.isAlphaPremultiplied();
             target = new BufferedImage(cm, raster, alphaPremultiplied, null);
-        } else
+        } else {
             target = new BufferedImage(targetW, targetH, type);
+        }
 
         Graphics2D g = target.createGraphics();
         // smoother than exlax:
@@ -206,14 +183,20 @@ public class ImageUtils {
         return target;
     }
 
-    public static void resize(File originalFile, File thumnailFile, String dimension) {
-        if ((StringUtils.isNotBlank(dimension)) && (StringUtils.contains(dimension, "x"))) {
-            int wdth = NumberUtils.createInteger(StringUtils.substringBefore(dimension, "x")).intValue();
-            int height = NumberUtils.createInteger(StringUtils.substringAfter(dimension, "x")).intValue();
-            resize(originalFile, thumnailFile, wdth, height, "png");
-        }
-    }
-
+    /**
+     * 图片缩放
+     * 
+     * @param originalFile
+     *            源文件
+     * @param thumnailFile
+     *            缩放后文件
+     * @param newWidth
+     *            缩放后宽度
+     * @param newHeight
+     *            缩放后高度
+     * @param format
+     *            缩放后图片格式
+     */
     public static void resize(File originalFile, File thumnailFile, int newWidth, int newHeight, String format) {
         try {
             resize(new FileInputStream(originalFile), new FileOutputStream(thumnailFile), newWidth, newHeight, format);
@@ -222,6 +205,20 @@ public class ImageUtils {
         }
     }
 
+    /**
+     * 图片缩放
+     * 
+     * @param originalStream
+     *            源文件
+     * @param thumnailStream
+     *            缩放后文件
+     * @param newWidth
+     *            缩放后宽度
+     * @param newHeight
+     *            缩放后高度
+     * @param format
+     *            缩放后图片格式
+     */
     public static void resize(InputStream originalStream, OutputStream thumnailStream, int newWidth, int newHeight, String format) {
         try {
             BufferedImage originalImage = ImageIO.read(originalStream);
@@ -319,77 +316,60 @@ public class ImageUtils {
     }
 
     /**
-     * 生成随机验证码
+     * 图片剪切
      * 
-     * @param outputStream
-     *            输出流
-     * @param allowValidateString
-     *            允许验证码中出现的字符串
-     * @return
+     * @param source
+     *            原图片
+     * @param startX
+     *            开始位置X坐标
+     * @param startY
+     *            开始位置Y坐标
+     * @param endX
+     *            结束位置X坐标
+     * @param endY
+     *            结束位置Y坐标
      */
-    public static String validateCode(OutputStream outputStream, String allowValidateString) {
-        int width = 60;
-        int height = 20;
+    public BufferedImage crop(BufferedImage source, int startX, int startY, int endX, int endY) {
+        int width = source.getWidth();
+        int height = source.getHeight();
 
-        BufferedImage buffImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB); // 缓冲区Image
-        Graphics2D g = buffImg.createGraphics(); // 获得在缓冲区Image中负责绘画的对象(画笔)他是Graphics的字类型
-
-        // 创建一个随机数生成器类。
-        Random random = new Random();
-
-        g.setColor(Color.decode("#ffffff"));
-        g.fillRect(0, 0, width, height); // 画一个填充的矩形背景颜色为白色
-
-        // 创建字体，字体的大小应该根据图片的高度来定。
-        Font font = new Font("Times New Roman", Font.PLAIN, 18);
-        // 设置字体。
-        g.setFont(font);
-
-        // 画边框。
-        // g.setColor(Color.blue);
-        // g.drawRect(0, 0, width - 1, height - 1);
-
-        // 随机产生160条干扰线，使图象中的认证码不易被其它程序探测到。
-        g.setColor(Color.GRAY);
-        for (int i = 0; i < 80; i++) {
-            int x = random.nextInt(width);
-            int y = random.nextInt(height);
-            int xl = random.nextInt(12);
-            int yl = random.nextInt(12);
-            g.drawLine(x, y, x + xl, y + yl);
+        if (startX == -1) {
+            startX = 0;
         }
 
-        // randomCode用于保存随机产生的验证码，以便用户登录后进行验证。
-        StringBuffer randomCode = new StringBuffer();
-        int red = 0, green = 0, blue = 0;
-
-        // 随机产生验证码。
-        for (int i = 0; i < 4; i++) {
-            // 得到随机产生的验证码数字。
-            int randomIndex = random.nextInt(allowValidateString.length());
-            if (randomIndex == 0)
-                randomIndex = 1;
-            String strRand = allowValidateString.substring(randomIndex - 1, randomIndex);
-
-            // 产生随机的颜色分量来构造颜色值，这样输出的每位数字的颜色值都将不同。
-            red = random.nextInt(110);
-            green = random.nextInt(50);
-            blue = random.nextInt(50);
-
-            // 用随机产生的颜色将验证码绘制到图像中。
-            g.setColor(new Color(red, green, blue));
-            g.drawString(strRand, 13 * i + 6, 16);
-
-            // 将产生的四个随机数组合在一起。
-            randomCode.append(strRand);
+        if (startY == -1) {
+            startY = 0;
         }
-        try {
-            ImageIO.write(buffImg, "jpeg", outputStream);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+
+        if (endX == -1) {
+            endX = width - 1;
         }
-        return randomCode.toString();
+
+        if (endY == -1) {
+            endY = height - 1;
+        }
+
+        BufferedImage result = new BufferedImage(endX - startX + 1, endY - startY + 1, 4);
+
+        for (int y = startY; y < endY; y++) {
+            for (int x = startX; x < endX; x++) {
+                int rgb = source.getRGB(x, y);
+                result.setRGB(x - startX, y - startY, rgb);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 获取图片的metadata信息
+     * 
+     * @param inputStream
+     * @return
+     * @throws ImageReadException
+     * @throws IOException
+     */
+    public static IImageMetadata getMetadataInfo(InputStream inputStream) throws ImageReadException, IOException {
+        return Imaging.getMetadata(inputStream, "");
     }
 
     /**
